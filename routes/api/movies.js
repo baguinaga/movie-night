18584d256882053d16b01691e2278bbd585e3f24
Chris Natividad <cnat3@yahoo.com>
2018-11-16 22:53:31 -0500

    movie.js

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -16,4 +16,31 @@ router
     });
   })
 
+
+  router
+  .route("/recommendations/:title")
+  .get(function(req,res){
+    axios
+    .get("https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_DB_API_KEY}&query=${req.params.title}&language=en-US&page=1&include_adult=false")
+    
+    .then(movie=> {
+      res.json(movie.data.id);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).send(err);
+    })
+//sending searched id for recommendations
+  .then(function(movieID) {
+    axios
+    .get(`https://api.themoviedb.org/3/movie/${movieID}/recommendations?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US&page=1`)
+    .then(movie => {
+      res.json(movie.data);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).send(err);
+    });
+  })
+})
 module.exports = router;
